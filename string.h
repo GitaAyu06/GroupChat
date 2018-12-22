e195811e7f586ccd937e614cc67e03e9babcae2c
Gita Ayu Salsabila <37178721+GitaAyu06@users.noreply.github.com>
2018-12-22 06:34:47 -0500

    Add files via upload

---
+++ string.h
@@ -0,0 +1,25 @@
+#ifndef STRING
+#define STRING
+
+void str_trim_lf (char*, int);
+void str_overwrite_stdout();
+
+#include <stdio.h>
+
+
+void str_trim_lf (char* arr, int length) {
+    int i;
+    for (i = 0; i < length; i++) { // trim \n
+        if (arr[i] == '\n') {
+            arr[i] = '\0';
+            break;
+        }
+    }
+}
+
+void str_overwrite_stdout() {
+    printf("\r%s", "> ");
+    fflush(stdout);
+}
+
+#endif // STRING
